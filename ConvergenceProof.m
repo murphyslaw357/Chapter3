887708cc425b02e89d5e4ede4082461b5a060752
Shaun Murphy <murphyslaw357@gmail.com>
2019-02-07 20:42:47 -0500

    2/7

--- ConvergenceProof.m
+++ ConvergenceProof.m
@@ -1,7 +1,8 @@
 clear
 clc
 
-conductorData=importfileAA('C:\Users\ctc\Documents\GitHub\NewtonRaphsonHeatBalance\ConductorInfo.csv');
+%conductorData=importfileAA('C:\Users\ctc\Documents\GitHub\NewtonRaphsonHeatBalance\ConductorInfo.csv');
+conductorData=importfileAA('/Users/Shaun/Documents/GitHub/NewtonRaphsonHeatBalance/ConductorInfo.csv');
 [conductorCount,~]=size(conductorData);
 conductorData.ResistanceACLowdegc=conductorData.ResistanceDCLowdegc;
 conductorData.ResistanceACLowdegcMeter=conductorData.ResistanceACLowdegc./conductorData.MetersperResistanceInterval;
@@ -14,14 +15,13 @@ phi=90;
 sigmab=5.6697e-8;
 alphas=0.9;
 spacer=10;
-deltas=zeros(conductorCount,1);
-delta1s=zeros(conductorCount,1);
-Cs=zeros(conductorCount,1);
+conductorData.deltas=zeros(conductorCount,1);
+conductorData.delta1s=zeros(conductorCount,1);
+conductorData.Cs=zeros(conductorCount,1);
 for c=1:conductorCount
     cdata=conductorData(c,:);
     maxcurrent=ceil(1.5*cdata.AllowableAmpacity);
     diam=cdata.DiamCompleteCable*0.0254;
-    disp(strcat(num2str(100*c/conductorCount),cellstr(cdata.CodeWord)))
     maxpsol=1050*diam*alphas;
 
     beta=(cdata.ResistanceACHighdegcMeter-cdata.ResistanceACLowdegcMeter)/(cdata.HighTemp-cdata.LowTemp);
@@ -35,9 +35,9 @@ for c=1:conductorCount
     limits=zeros(a1*a2*a2,3);
     counter=0;
     rerun=1;
-    delta=100;
-    delta1=100;
-    Cs(c)=-1*realmax;
+    conductorData.delta(c)=0.5;
+    conductorData.delta1(c)=2;
+    conductorData.Cs(c)=-1*realmax;
     while(rerun)
         rerun=0;
         for psol=0:maxpsol/spacer:maxpsol
@@ -50,15 +50,15 @@ for c=1:conductorCount
                          phi=90*pi/180;
                          [root,~,~,~,~,~,~] =GetTempNewtonFullDiagnostic(imagnitude,ambtemp,H,diam,phi,Vw,alpha,beta,epsilons,psol);
                          counter=counter+1;
-                         dist=ceil((GuessTc+delta1)-(root-delta));
+                         dist=ceil((GuessTc+conductorData.delta1(c))-(root-conductorData.delta(c)));
                          ftracker=zeros(dist,1);
                          fprimetracker=zeros(dist,1);
                          pcontracker=zeros(dist,1);
                          pconprimetracker=zeros(dist,1);
                          temp=zeros(dist,1);
                          fcounter=0;
-                         for Tcc=root-delta:GuessTc+delta1
-                             fcounter=fcounter+1;
+                         for Tcc=root-conductorData.delta(c):GuessTc+conductorData.delta1(c)
+                            fcounter=fcounter+1;
                             [Tc,I2R,I2Rprime,Prad,Pradprime,Pradprimeprime,Pcon,Pconprime,Pconprimeprime] =GetTempNewtonFullDiagnosticFirstIteration(imagnitude,ambtemp,H,diam,phi,Vw,alpha,beta,epsilons,psol,Tcc);
                             h=I2R+psol-Prad-Pcon;
                             hprime=I2Rprime-Pradprime-Pconprime;
@@ -70,18 +70,18 @@ for c=1:conductorCount
                             temp(fcounter)=Tcc;
                             gprime=abs(1-((hprime*hprime-h*hprimeprime)/(hprime^2)));
                             g=Tcc-h/hprime;
-                            if(g<root-delta)
-                                delta=root-g;
+                            if(g<root-conductorData.delta(c))
+                                conductorData.delta(c)=root-g;
                                 rerun=1;
-                            elseif(g>GuessTc+delta1)
-                                delta1=g-GuessTc;
+                            elseif(g>GuessTc+conductorData.delta1(c))
+                                conductorData.delta1(c)=g-GuessTc;
                                 rerun=1;
                             end
                             if(rerun) 
                                 break 
                             end
-                            if(gprime>Cs(c))
-                                Cs(c)=gprime;
+                            if(gprime>conductorData.Cs(c))
+                                conductorData.Cs(c)=gprime;
                             end
                          end
                          if(dist>20)
@@ -101,10 +101,7 @@ for c=1:conductorCount
             end
         end
     end
-    deltas(c)=delta;
-    delta1s(c)=delta1;
-    disp(delta)
-    disp(delta1)    
+    disp(strcat(num2str(conductorData.delta(c)),',',num2str(conductorData.delta1(c)),',',num2str(100*c/conductorCount),',',cellstr(cdata.CodeWord)))
 end
 conductorData.deltas=deltas;
 conductorData.delta1s=delta1s;
