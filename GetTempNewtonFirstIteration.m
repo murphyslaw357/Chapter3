23c94ebf30da08c8451f521e3bea9459d9e1c9c5
murphyslaw357 <murphyslaw357@gmail.com>
2019-02-15 17:45:00 -0500

    2/15

--- GetTempNewtonFirstIteration.m
+++ GetTempNewtonFirstIteration.m
@@ -1,4 +1,4 @@
-function [GuessTc,I2R,I2Rprime,Prad,PradPrime,Pcon,PconPrime,A,m,C,n] =GetTempNewtonFirstIteration(I,Ta,H,D,phi,Vw,alpha,beta,epsilons,Psol,GuessTc,fGrPr,fReNu,fNuRe)
+function [GuessTc,I2R,I2Rprime,Prad,PradPrime,PradPrimePrime,Pcon,PconPrime] =GetTempNewtonFirstIteration(I,Ta,H,D,phi,Vw,alpha,beta,epsilons,Psol,GuessTc,fGrPr,fReNu,fNuRe)
     %I - RMS steady-state load current - amps
     %Ta - ambient temperature - degc
     %H - conductor elevation - meters
@@ -18,6 +18,7 @@ function [GuessTc,I2R,I2Rprime,Prad,PradPrime,Pcon,PconPrime,A,m,C,n] =GetTempNe
     Prad=pi*D*sigmab*epsilons*(((GuessTc+273)^4)-((Ta+273)^4));
     %Prad=pi*D*sigmab*epsilons*(((GuessTc+273)^4)-0.5*((Tsky+273)^4)-0.5*((Tg+273)^4));
     PradPrime=4*pi*D*sigmab*epsilons*(GuessTc+273)^3;
+    PradPrimePrime=12*pi*D*sigmab*epsilons*(GuessTc+273)^2;
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%JOULE HEATING%%%%%%%%%%%%%%%%%%%%%%%
     I2Rprime=beta*IIstar;
     Resistance=beta*GuessTc+alpha;
@@ -28,36 +29,19 @@ function [GuessTc,I2R,I2Rprime,Prad,PradPrime,Pcon,PconPrime,A,m,C,n] =GetTempNe
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%CONVECTIVE
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%COOLING/HEATING%%%%%%%%%
 %     TfilmkPrime=1/2;
-%     PrPrime=-1.25e-4;
+    PrPrime=-1.25e-4;
     GuessTfilm=(GuessTc+Ta)/2;
     vf=((1.32e-5)+(9.5e-8)*GuessTfilm)*((1-((6.5e-3)*H)/288.16)^-5.2561);
-%     vfPrime=(4.75e-8)*((1-((6.5e-3)*H)/288.16)^-5.2561);
+    vfPrime=(4.75e-8)*((1-((6.5e-3)*H)/288.16)^-5.2561);
     
-%     Relim1=(0.437/0.565)^(1/(0.136-0.0895));
-%     Relim2=(0.565/0.8)^(1/(0.280-0.136));
-%     Relim3=(0.8/0.795)^(1/(0.384-0.280));
-%     Relim4=(0.795/0.583)^(1/(0.471-0.384));
-%     Relim5=(0.583/0.148)^(1/(0.633-0.471));
-%     Relim6=(0.148/0.0208)^(1/(0.814-0.633));
-%     Nulim1=0.437*(Relim1^0.0895);
-%     Nulim2=0.565*(Relim2^0.136);
-%     Nulim3=0.800*(Relim3^0.280);
-%     Nulim4=0.795*(Relim4^0.384);
-%     Nulim5=0.583*(Relim5^0.471);
-%     Nulim6=0.148*(Relim6^0.633);
-%     GrPrlim1=(0.675/0.889)^(1/(0.088-0.058));
-%     GrPrlim2=(0.889/1.02)^(1/(0.148-0.088));
-%     GrPrlim3=(1.02/0.850)^(1/(0.188-0.148));
-%     GrPrlim4=(0.850/0.480)^(1/(0.250-0.188));
-%     GrPrlim5=(0.480/0.125)^(1/(0.333-0.250));
-
     Tfilmk=GuessTfilm+273;
 
     Gr=(g*(D^3)*abs(GuessTc-Ta))/(Tfilmk*(vf^2));    
     Lambdaf=(2.42e-2)+(7.2e-5)*GuessTfilm;
     LambdafPrime=3.6e-5;
-%     GrPrime=g*(D^3)*(Tfilmk*(vf)-abs(GuessTc-Ta)*(TfilmkPrime*vf+Tfilmk*2*vfPrime))/...
-%             ((Tfilmk^2)*(vf^3));
+    TfilmkPrime=1/2;
+    GrPrime=g*(D^3)*(Tfilmk*(vf)-abs(GuessTc-Ta)*(TfilmkPrime*vf+Tfilmk*2*vfPrime))/...
+            ((Tfilmk^2)*(vf^3));
     Pr=0.715-(2.5e-4)*GuessTfilm;
     GrPr=Gr*Pr;
     A=0;
@@ -67,56 +51,38 @@ function [GuessTc,I2R,I2Rprime,Prad,PradPrime,Pcon,PconPrime,A,m,C,n] =GetTempNe
     if(GuessTc~=Ta && Vw==0)
         %pure natural convection 
         Nudf=fGrPr(GrPr);
-        NudfPrime=differentiate(fGrPr,GrPr); 
+        [NudfPrime,NudfPrimePrime]=differentiate(fGrPr,GrPr); 
+        NudfPrime=NudfPrime*(Gr*PrPrime+GrPrime*Pr);
+        NudfPrimePrime=
         Pcon=pi*Nudf*Lambdaf*(GuessTc-Ta);
-        PconPrime=pi*(Nudf*Lambdaf+LambdafPrime*Nudf*(GuessTc-Ta)+NudfPrime*Lambdaf*(GuessTc-Ta));
+        PconPrime=pi*(Nudf*Lambdaf+(GuessTc-Ta)*(LambdafPrime*Nudf+NudfPrime*Lambdaf));
+        PconPrimePrime=pi*(NudfPrime*Lambdaf+Nudf*LambdafPrime+(LambdafPrime*Nudf+NudfPrime*Lambdaf)+(GuessTc-Ta)*(LambdafPrime*NudfPrime+NudfPrimePrime*Lambdaf+Nudf*LambdafPrime));
     elseif(GuessTc~=Ta && Vw ~=0)
     %mixed convection
         Nudf=fGrPr(GrPr);
+        NudfPrime=differentiate(fGrPr,GrPr);
         Req=fNuRe(Nudf);
-%         %lookup C and n
-%         C=0;
-%         n=0;
-%         if(Nudf<=Nulim1)
-%             C=0.437;
-%             n=0.0895;
-%         elseif(Nudf>Nulim1 && Nudf<=Nulim2)
-%             C=0.565;
-%             n=0.136;
-%         elseif(Nudf>Nulim2 && Nudf<=Nulim3)
-%             C=0.8;
-%             n=0.28;
-%         elseif(Nudf>Nulim3 && Nudf<=Nulim4)
-%             C=0.795;
-%             n=0.384;
-%         elseif(Nudf>Nulim4 && Nudf<=Nulim5)
-%             C=0.583;
-%             n=0.471;
-%         elseif(Nudf>Nulim5 && Nudf<=Nulim6)
-%             C=0.148;
-%             n=0.633;
-%         elseif(Nudf>Nulim6)
-%             C=0.0208;
-%             n=0.814;
-%         end
-%         if(C==0)
-%             msg='error';
-%             error(msg)
-%         end
-%         Req=(Nudf/C)^(1/n);
+        ReqPrime=differentiate(fNuRe,Nudf)*NudfPrime*(Gr*PrPrime+GrPrime*Pr);
         Re=sin(phi)*Vw*D/vf;
+        RePrime=-(sin(phi)*Vw*D*vfPrime)/(vf^2);
         Reeff=sqrt((Re^2)+(Req^2));
-        NueffPrime=differentiate(fReNu,Reeff);
+        Top=Re*RePrime+Req*ReqPrime;% (GrPrime*Pr+Gr*PrPrime);
+        
+        ReeffPrime=Top/Reeff;
+        [NueffPrime,NueffPrimePrime]=differentiate(fReNu,Reeff);
         Nueff=fReNu(Reeff);
         Pcon=pi*Nueff*Lambdaf*(GuessTc-Ta);
-        PconPrime=pi*(Nueff*Lambdaf+LambdafPrime*Nueff*(GuessTc-Ta)+NueffPrime*Lambdaf*(GuessTc-Ta));
+        PconPrime=pi*(Nueff*Lambdaf+(GuessTc-Ta)*(LambdafPrime*Nueff+NueffPrime*ReeffPrime*Lambdaf));
+        %PconPrimePrime=pi*NueffPrimePrime*Lambdaf*(GuessTc-Ta)+2*pi*NueffPrime*(LambdafPrime*(GuessTc-Ta)+Lambdaf)+2*pi*Nueff*LambdafPrime;
     elseif(GuessTc==Ta && Vw~=0)
     %pure forced    
         Re=sin(phi)*Vw*D/vf;  
-        NuPrime=differentiate(fReNu,Re);
+        RePrime=-(sin(phi)*Vw*D*vfPrime)/(vf^2);
+        [NuPrime,NuPrimePrime]=differentiate(fReNu,Re);
         Nu=fReNu(Re);
         Pcon=pi*Nu*Lambdaf*(GuessTc-Ta);
-        PconPrime=pi*(Nu*Lambdaf+LambdafPrime*Nu*(GuessTc-Ta)+NuPrime*Lambdaf*(GuessTc-Ta));
+        PconPrime=pi*(Nu*Lambdaf+(GuessTc-Ta)*(LambdafPrime*Nu+NuPrime*RePrime*Lambdaf));
+        %PconPrimePrime=pi*NuPrimePrime*Lambdaf*(GuessTc-Ta)+2*pi*NuPrime*(LambdafPrime*(GuessTc-Ta)+Lambdaf)+2*pi*Nu*LambdafPrime;
     else
     %no convection at all    
         Pcon=0;
