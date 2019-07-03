3fc536c878c02bf74216ed1074e18a56fc614f2a
murphyslaw357 <murphyslaw357@gmail.com>
2019-07-03 13:57:15 -0400

    703

--- Figure3_2.m
+++ Figure3_2.m
@@ -1,3 +1,5 @@
+clear
+clc
 close all
 
 if(ispc==1)
@@ -8,14 +10,27 @@ elseif(isunix==1)
     foldersource='/mnt/HA/groups/nieburGrp/Shaun/Chapter3/';
 end
 
-conductorData=importfileAB(strcat(foldersource,'conductorData.csv'));
+conductorData=importfile46(strcat(foldersource,'conductorDataResults.csv'));
 [conductorCount,~] = size(conductorData);
 
 cMax=conductorData.Cmax;
 cMin=conductorData.Cmin;
+subplot(2,1,1);
 plot(cMax)
 ylim([0 1])
-hold on
+yL = get(gca,'YLim');
+line([68 68],yL,'LineStyle','--','Color','r');
+line([132 132],yL,'LineStyle','--','Color','r');
+line([161 161],yL,'LineStyle','--','Color','r');
+line([175 175],yL,'LineStyle','--','Color','r');
+line([230 230],yL,'LineStyle','--','Color','r');
+line([302 302],yL,'LineStyle','--','Color','r');
+line([339 339],yL,'LineStyle','--','Color','r');
+
+legend('Maximum C')
+xlabel('Conductor Index')
+ylabel('Convergence Coefficient')
+subplot(2,1,2); 
 plot(cMin)
 yL = get(gca,'YLim');
 line([68 68],yL,'LineStyle','--','Color','r');
@@ -25,7 +40,7 @@ line([175 175],yL,'LineStyle','--','Color','r');
 line([230 230],yL,'LineStyle','--','Color','r');
 line([302 302],yL,'LineStyle','--','Color','r');
 line([339 339],yL,'LineStyle','--','Color','r');
-legend('Maximum C','Minimum C')
+legend('Minimum C')
 xlabel('Conductor Index')
 ylabel('Convergence Coefficient')
 print(gcf,strcat(foldersource,'Figure3_2.jpg'),'-r1200','-djpeg')
